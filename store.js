bd8d821f38c8961341996d4486b22fbd006817ec
Qian Ye <yeqian.zju@gmail.com>
2014-11-05 09:48:35 +0800

    add store.js

---
+++ store.js
@@ -0,0 +1,111 @@
+var data = {
+  ios: [{
+    link: 'http://item.taobao.com/item.htm?spm=a230r.1.0.0.TZRkLA&id=42050680607&ns=1&abbucket=10#detail',
+    imglink: 'http://gd4.alicdn.com/bao/uploaded/i4/TB1ir_7GFXXXXbHXFXXXXXXXXXX_!!0-item_pic.jpg_400x400.jpg',
+    des: 'air story标准型电池',
+    origin: '',
+    sale: '80.00'
+  }, {
+    link: 'http://item.taobao.com/item.htm?spm=a230r.1.0.0.TZRkLA&id=42050468204&ns=1&abbucket=10#detail',
+    imglink: 'http://gd1.alicdn.com/bao/uploaded/i1/TB1lbz7GFXXXXb2XFXXXXXXXXXX_!!0-item_pic.jpg_400x400.jpg',
+    des: 'air story基础型滤网',
+    origin: '',
+    sale: '40.00'
+  }, {
+    link: 'http://s.click.taobao.com/t?e=m%3D2%26s%3DYU%2BTNlXvfF0cQipKwQzePOeEDrYVVa64Qih%2F7PxfOKS5VBFTL4hn2SOiiuezDuszrumJQoe%2FxcN4%2BDUoFL30LER2%2FcAgy3eptkjwnoSGqUBi8N%2BXN7AYzTQ38ghBkTNTWGnUgpJO7%2B4Qr4Mvpe6v0iGFCzYOOqAQ',
+    imglink: 'http://img01.taobaocdn.com/bao/uploaded/i1/10800043045868330/T1h9k2FbpdXXXXXXXX_!!0-item_pic.jpg_430x430.jpg',
+    des: '特价 双IC芯片方案 10.8V11.1V12V 12.6V2A锂电池充电器 充满变灯',
+    origin: '15.00',
+    sale: '14.00'
+  }, {
+    link: 'http://redirect.simba.taobao.com/rd?w=unionnojs&f=http%3A%2F%2Fai.taobao.com%2Fauction%2Fedetail.htm%3Fe%3DjQebmAFlV0UjmraEDZVrLhtbeCW%252FpXvyRfbtQaszFx6LltG5xFicOdXrTUTgh9sMDPIwxrc30rgx5xFFx04TdbxCQwsdJ1VWisBnzZ6WAYOYhcyKev2R7VRmtaud%252B0v%252BgXGApso33BVR0I2JFdSX0A%253D%253D%26ptype%3D100010%26from%3Dbasic&k=5ccfdb950740ca16&c=un&b=alimm_0&p=mm_100523592_7864421_26738537',
+    imglink: 'http://img01.taobaocdn.com/bao/uploaded/i1/T1yHPWFiBxXXXXXXXX_!!0-item_pic.jpg_430x430.jpg',
+    des: '先科 带开关一拖三点烟器 汽车双USB一分三电源转换器 车载充电器',
+    origin: '90.00',
+    sale: '36.00'
+  }],
+  android: [{
+    link: 'http://item.taobao.com/item.htm?spm=a230r.1.0.0.TZRkLA&id=42050680607&ns=1&abbucket=10#detail',
+    imglink: 'http://gd4.alicdn.com/bao/uploaded/i4/TB1ir_7GFXXXXbHXFXXXXXXXXXX_!!0-item_pic.jpg_400x400.jpg',
+    des: 'air story标准型电池',
+    origin: '',
+    sale: '80.00'
+  }, {
+    link: 'http://item.taobao.com/item.htm?spm=a230r.1.0.0.TZRkLA&id=42050468204&ns=1&abbucket=10#detail',
+    imglink: 'http://gd1.alicdn.com/bao/uploaded/i1/TB1lbz7GFXXXXb2XFXXXXXXXXXX_!!0-item_pic.jpg_400x400.jpg',
+    des: 'air story基础型滤网',
+    origin: '',
+    sale: '40.00'
+  }, {
+    link: 'http://s.click.taobao.com/t?e=m%3D2%26s%3DScwm2iH5QBQcQipKwQzePOeEDrYVVa64Qih%2F7PxfOKS5VBFTL4hn2Z%2Fu6XUKtBNX6EFRCN7EKmx4%2BDUoFL30LP9HPpEiURghHI0qbQNuPotHxejAxTjXYjQ38ghBkTNTWGnUgpJO7%2B4Qr4Mvpe6v0iGFCzYOOqAQ',
+    imglink: 'http://img01.taobaocdn.com/bao/uploaded/i1/10800043045868330/T1h9k2FbpdXXXXXXXX_!!0-item_pic.jpg_430x430.jpg',
+    des: '特价 双IC芯片方案 10.8V11.1V12V 12.6V2A锂电池充电器 充满变灯',
+    origin: '15.00',
+    sale: '14.00'
+  }, {
+    link: 'http://s.click.taobao.com/t?e=m%3D2%26s%3DOyIC4LtcpdUcQipKwQzePOeEDrYVVa64Qih%2F7PxfOKS5VBFTL4hn2Z%2Fu6XUKtBNXJhSgLssdd1Z4%2BDUoFL30LP9HPpEiURghyG2Xh5CAD%2Bc8i3P7EErua9BfSc1yHiA7I8LMQdGH1N4MpjOeUsJ7%2FMYOae24fhW0',
+    imglink: 'http://img01.taobaocdn.com/bao/uploaded/i1/T1yHPWFiBxXXXXXXXX_!!0-item_pic.jpg_430x430.jpg',
+    des: '先科 带开关一拖三点烟器 汽车双USB一分三电源转换器 车载充电器',
+    origin: '90.00',
+    sale: '36.00'
+  }],
+  def: [{
+    link: 'http://item.taobao.com/item.htm?spm=a230r.1.0.0.TZRkLA&id=42050680607&ns=1&abbucket=10#detail',
+    imglink: 'http://gd4.alicdn.com/bao/uploaded/i4/TB1ir_7GFXXXXbHXFXXXXXXXXXX_!!0-item_pic.jpg_400x400.jpg',
+    des: 'air story标准型电池',
+    origin: '',
+    sale: '80.00'
+  }, {
+    link: 'http://item.taobao.com/item.htm?spm=a230r.1.0.0.TZRkLA&id=42050468204&ns=1&abbucket=10#detail',
+    imglink: 'http://gd1.alicdn.com/bao/uploaded/i1/TB1lbz7GFXXXXb2XFXXXXXXXXXX_!!0-item_pic.jpg_400x400.jpg',
+    des: 'air story基础型滤网',
+    origin: '',
+    sale: '40.00'
+  }, {
+    link: 'http://baidu.com',
+    imglink: 'http://dummyimage.com/150x100/b3b1b3/ffffff.png&text=ooo',
+    des: '先科 带开关 一拖三点烟器汽车双USB电源一拖二分配器 车载点烟器',
+    origin: '72.00-84.00',
+    sale: '36.00-42.00'
+  }, {
+    link: 'http://baidu.com',
+    imglink: 'http://dummyimage.com/150x100/b3b1b3/ffffff.png&text=ooo',
+    des: '先科 带开关 一拖三点烟器汽车双USB电源一拖二分配器 车载点烟器',
+    origin: '72.00-84.00',
+    sale: '36.00-42.00'
+  }]
+}
+
+var browser = (function() {
+  var u = navigator.userAgent
+  return {
+    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+    android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1
+  }
+})()
+
+function tpl(tmpl, data) {
+  return tmpl.replace(/<%(.+?)%>/g, function(all, key) {
+    return data[key] ? data[key] : ''
+  })
+}
+
+var html = ''
+var dataKey = 'def'
+if (browser.ios) {
+  dataKey = 'ios'
+} else if (browser.android) {
+  dataKey = 'android'
+}
+
+var tmpl = document.getElementById('tmpl').innerText
+html += '<div class="row">'
+html += tpl(tmpl, data[dataKey][0])
+html += tpl(tmpl, data[dataKey][1])
+html += '</div>'
+html += '<div class="row">'
+html += tpl(tmpl, data[dataKey][2])
+html += tpl(tmpl, data[dataKey][3])
+html += '</div>'
+
+document.getElementById('container').innerHTML = html
